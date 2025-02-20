2c992db768d01ef4b0c2c1e56e6f2129f2464a96
Sara Jahedazad <114187084+sarajahedazad@users.noreply.github.com>
2025-02-19 22:08:19 -0500

    Update test_bisection_method.py

--- tests/test_bisection_method.py
+++ tests/test_bisection_method.py
@@ -8,11 +8,59 @@ def test_bisection_method():
   abs_tol = 10**(-10)
   rel_tol = 10**(-10)
   max_iter = 50
-  verbose = False
+  verbose = True
   
   a = 1
   b = 4
   known_answer = 2
+  
+  solver = bm.BisectionSolver(abs_tol = abs_tol, rel_tol = rel_tol, max_iter = max_iter, verbose = verbose)
+  calculated_answer = solver.solve( func_example, a, b )
+
+  assert np.isclose( known_answer , calculated_answer )
+
+  def func_example(x):
+    return math.log(x+8)
+  abs_tol = 10**(-10)
+  rel_tol = 10**(-10)
+  max_iter = 50
+  verbose = True
+  
+  a = -7
+  b = 1
+  known_answer = -7
+  
+  solver = bm.BisectionSolver(abs_tol = abs_tol, rel_tol = rel_tol, max_iter = max_iter, verbose = verbose)
+  calculated_answer = solver.solve( func_example, a, b )
+
+  assert np.isclose( known_answer , calculated_answer )
+
+  def func_example(x):
+    return 
+  abs_tol = 10**(-10)
+  rel_tol = 10**(-10)
+  max_iter = 50
+  verbose = True
+  
+  a = -7
+  b = 2
+  known_answer = -7
+  
+  solver = bm.BisectionSolver(abs_tol = abs_tol, rel_tol = rel_tol, max_iter = max_iter, verbose = verbose)
+  calculated_answer = solver.solve( func_example, a, b )
+
+  assert np.isclose( known_answer , calculated_answer )
+
+  def func_example(x):
+    return x**3 - x**2 + x - 1
+  abs_tol = 10**(-10)
+  rel_tol = 10**(-10)
+  max_iter = 50
+  verbose = True
+  
+  a = -5
+  b = 1
+  known_answer = 1
   
   solver = bm.BisectionSolver(abs_tol = abs_tol, rel_tol = rel_tol, max_iter = max_iter, verbose = verbose)
   calculated_answer = solver.solve( func_example, a, b )
@@ -35,7 +83,7 @@ def test_samesign_error():
 def test_max_iter():
   def func_example(x):
     return x**3 - 4 *x
-    abs_tol = 10**(-10)
+  abs_tol = 10**(-10)
   rel_tol = 10**(-10)
   max_iter = 10
   verbose = False
