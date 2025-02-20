aa4d71022e3c5ed732e96e89f23364e3e20e7675
Sara Jahedazad <114187084+sarajahedazad@users.noreply.github.com>
2025-02-19 21:33:35 -0500

    Update test_bisection_method.py

--- tests/test_bisection_method.py
+++ tests/test_bisection_method.py
@@ -19,3 +19,16 @@ def test_bisection_method():
 
   assert np.isclose( known_answer , calculated_answer )
 
+def test_samesign_error():
+  def func_example1(x):
+      return x**3 - 4 * x
+  abs_tol = 10**(-10)
+  rel_tol = 10**(-10)
+  max_iter = 50
+  verbose = False
+  a = 1
+  b = 4
+  solver = bm.BisectionSolver(abs_tol = abs_tol, rel_tol = rel_tol, max_iter = max_iter, verbose = verbose)
+  with pytest.raises(bm.OutofRangeError):
+      solver.solve( func_example1, a, b )
+
